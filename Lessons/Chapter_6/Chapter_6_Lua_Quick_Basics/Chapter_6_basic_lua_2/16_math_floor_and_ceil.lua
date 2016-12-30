03b7baca0bb00382675cc47c78fde2403c19033e
Zane Kukta <zanekukta@protonmail.ch>
2016-12-30 02:40:52 -0700

    Add files via upload

---
+++ Lessons/Chapter_6/Chapter_6_Lua_Quick_Basics/Chapter_6_basic_lua_2/16_math_floor_and_ceil.lua
@@ -0,0 +1,6 @@
+--math.floor is for rounding down and math.ceil is for rounding up
+print ("1.5 rounded down: "..math.floor(1.5))
+print ("1.5 rounded  up: "..math.ceil(1.5))
+
+print ("-1.5 rounded down: "..math.floor(-1.5))
+print ("-1.5 rounded  up: "..math.ceil(-1.5))
